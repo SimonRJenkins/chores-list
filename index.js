3eeb2929b1a4c88ab1647e4da6a54020c477f13d
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-10-22 00:05:10 +0100

    Add chores
    A new chore div is added when the add chore button is pressed
    A new chore is deleted when it is clicked on
    The add chore button is disabled when the input field is empty
    The remove chore button clears all chores that are currently displayed
    Chores are saved and loaded through localStorage

---
+++ index.js
@@ -0,0 +1,96 @@
+const choresEl = document.querySelector(".chores")
+const choreInput = document.getElementById("chore-input")
+const addChoreButton = document.getElementById("add-chore")
+const removeChoresButton = document.getElementById("remove-chores")
+
+function addChoreToList(choreText) {
+
+    const newChore = document.createElement('div')
+    newChore.className = 'chore'
+    newChore.innerHTML = `<p id="chore-text">${choreText}</p>`
+
+    choresEl.appendChild(newChore)
+    saveChores(choreText)
+
+    choreInput.value = ""
+
+    newChore.addEventListener("click", function () {
+        choresEl.removeChild(newChore)
+
+        removeChoreFromLocalStorage(choreText)
+    })
+
+}
+
+function saveChores() {
+    if (addChoreButton.disabled) {
+        return;
+    }
+
+    const choreText = choreInput.value
+
+
+    const storedChores = JSON.parse(localStorage.getItem('chores')) || []
+    storedChores.push(choreText);
+
+    localStorage.setItem('chores', JSON.stringify(storedChores))
+
+    choreInput.value = ""
+    addChoreButton.disabled = true
+
+}
+
+function removeChoreFromLocalStorage(choreText) {
+    const storedChores = getStoredChores()
+    const updatedChores = storedChores.filter(chore => chore !== choreText)
+    localStorage.setItem("chores", JSON.stringify(updatedChores))
+}
+
+function getStoredChores() {
+    const storedData = localStorage.getItem("chores")
+
+    if (storedData) {
+        try {
+            return JSON.parse(storedData)
+        } catch (error) {
+            console.error("Error parsing JSON from localStorage:", error)
+            return []
+        }
+    }
+    return []
+}
+
+function loadChores() {
+    const storedChores = getStoredChores()
+    storedChores.forEach(choreText => {
+        addChoreToList(choreText)
+    })
+}
+
+
+function validateInput() {
+    if (choreInput.value === "") {
+        addChoreButton.disabled = true
+    } else {
+        addChoreButton.disabled = false
+    }
+}
+
+choreInput.addEventListener("input", validateInput)
+
+window.addEventListener("beforeunload", function () {
+    saveChores();
+})
+
+window.addEventListener("load", function () {
+    validateInput()
+    loadChores()
+})
+
+
+removeChoresButton.addEventListener("click", function () {
+    while (choresEl.firstChild) {
+        choresEl.removeChild(choresEl.firstChild)
+    }
+    localStorage.removeItem("chores")
+})
